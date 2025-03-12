f77096bf3f0cffb67a2252a5de2425fda5b2d2c3
Armando Azevedo <armandojpazevedo@gmail.com>
2025-03-12 19:44:36 +0000

    Books Data Structure based on a JSON

---
+++ Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rest.Models;
+using Rest.Services;
+
+namespace Rest.Controllers
+{
+    [Route("api/books")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly BookService _bookService;
+
+        public BookController()
+        {
+            _bookService = new BookService();
+        }
+
+        [HttpGet]
+        public ActionResult<List<BookModel>> Get()
+        {
+            return Ok(_bookService.GetBooks());
+        }
+    }
+}
---
+++ Services/BookService.cs
@@ -0,0 +1,25 @@
+﻿using Rest.Models;
+using System.Text.Json;
+
+public class BookService
+{
+    private readonly string _filePath;
+
+    public BookService()
+    {
+        var basePath = AppDomain.CurrentDomain.BaseDirectory;
+        _filePath = Path.Combine(basePath, "Data", "dados.json");
+    }
+
+    public List<BookModel> GetBooks()
+    {
+        if (!File.Exists(_filePath))
+        {
+            return new List<BookModel>();
+        }
+
+        var jsonData = File.ReadAllText(_filePath);
+        var books = JsonSerializer.Deserialize<List<BookModel>>(jsonData);
+        return books ?? new List<BookModel>();
+    }
+}
