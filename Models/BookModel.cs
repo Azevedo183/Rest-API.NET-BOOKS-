d0e5b3e55c7205304e9d1b9b5f0835011646adb7
Armando Azevedo <armandojpazevedo@gmail.com>
2025-03-12 19:33:39 +0000

    Create the Book Model

---
+++ Models/BookModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Rest.Models
+{
+    public class BookModel
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string description { get; set; }
+        public int published_year { get; set; }
+    }
+}
